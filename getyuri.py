062b91976780491f1a65f5dde52e57a41f88881d
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-04-16 21:55:10 +0900

    ちゃんととまるようになたよー

--- getyuri.py
+++ getyuri.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import re
 from time import sleep
 import pickle
-baseurl = 'https://matome.naver.jp'
 
 
-def get_image_urls(url='/odai/2134131257162744701/2143315691256592103'):
+def get_image_urls(url):
+    baseurl = 'https://matome.naver.jp'
     url = baseurl + url
     response = requests.get(url)
     try:
@@ -17,16 +17,17 @@ def get_image_urls(url='/odai/2134131257162744701/2143315691256592103'):
         soup = BeautifulSoup(response.text, 'html5lib')
         print('html5lib parsed!')
 
-    with open('YuriTakami.txt', 'a') as f:
+    with open('YuriTakami2.txt', 'a') as f:
         f.write(url + '\n')
 
     next_url = soup.find_all(class_='mdMTMEnd01Pagination01Next')
     next_url = re.sub('\".*a>', '', re.sub('<a.*href=\"', '', str(next_url)))
-    if not next_url:
+    if not next_url[1:-1]:
         return 0
     sleep(2)
     get_image_urls(next_url[1:-1])
 
 
 if __name__ == '__main__':
-    get_image_urls()
+    start = '/odai/2134131257162744701/2143315691256592103'
+    get_image_urls(start)
