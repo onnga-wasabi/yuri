b3cf84168d1342f1e1c98e6fd664bef78a1611f8
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-04-17 19:44:25 +0900

    画像のURLから保存するようにしたよー，けど例外ばっかだよー

--- getyuri.py
+++ getyuri.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 import re
 from time import sleep
 import pickle
+import os
+import cv2
+import numpy as np
 
 
-def get_image_urls(url):
+def crawl_urls(url):
     baseurl = 'https://matome.naver.jp'
     url = baseurl + url
     response = requests.get(url)
@@ -25,9 +28,43 @@ def get_image_urls(url):
     if not next_url[1:-1]:
         return 0
     sleep(2)
-    get_image_urls(next_url[1:-1])
+    crawl_urls(next_url[1:-1])
+
+
+def save_image(url, dname, fname):
+    content = get_image(url)
+    fname = os.path.join(dname, fname + '.png')
+    with open(fname, 'wb') as f:
+        try:
+            f.write(content)
+        except:
+            return 0
+    return 0
+
+
+def get_image(url):
+    response = requests.get(url)
+    try:
+        soup = BeautifulSoup(response.text, 'lxml')
+        #print('lxml parsed!')
+    except:
+        soup = BeautifulSoup(response.text, 'html5lib')
+        print('html5lib parsed!')
+    try:
+        url = soup.find_all(class_='mdMTMEnd01Img01')[0]
+    except:
+        return 0
+    url = re.sub('<a href=\"', '', re.sub('\" target.*/></a>', '', str(url.a)))
+    return requests.get(url).content
+
+
+def save_images(urls):
+    return 0
 
 
 if __name__ == '__main__':
-    start = '/odai/2134131257162744701/2143315691256592103'
-    get_image_urls(start)
+    txt = 'YuriTakami.txt'
+    with open(txt, 'r') as f:
+        urls = f.readlines()
+    for i, url in enumerate(urls):
+        save_image(url, 'data/yuri', str(i))
